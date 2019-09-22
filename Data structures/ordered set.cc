64f42aac039f4daf549b8c098df84c78a87decfa
Bohoty <49064312+Bohoty@users.noreply.github.com>
2019-09-22 14:41:28 +0200

    Create ordered set.cc

---
+++ Data structures/ordered set.cc
@@ -0,0 +1,6 @@
+#include <ext/pb_ds/assoc_container.hpp> 
+#include <ext/pb_ds/tree_policy.hpp> 
+using namespace __gnu_pbds; 
+#define ordered_set tree<int, null_type,less<int>, rb_tree_tag,tree_order_statistics_node_update> 
+// find_by_order() returns iterator
+// order_of_key() returns the number of elements less than this key
