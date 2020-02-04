ad125483a64d7ae65dffd57faeffae69d88fbe2d
Bohoty <49064312+Bohoty@users.noreply.github.com>
2020-02-04 10:13:11 +0000

    Update Prefix Function.cc

--- Strings/Prefix Function.cc
+++ Strings/Prefix Function.cc
@@ -46,9 +46,25 @@ int diff_substrings(string &s, char c){ // not verified yet
 }
 
 // compressing a string: the min length of a string that you can concanecate diff occurences of it to obtain s
-int comp_len(string s)
+int comp_len(string s) { // not verified
   int k = n - pi[n - 1];
   return (n % k? n : k);
+}
+
+// compute the automation s + # + t
+void compute_automaton(string s, vector<vector<int>>& aut) {
+    s += '#';
+    int n = s.size();
+    vector<int> pi = prefix_function(s);
+    aut.assign(n, vector<int>(26));
+    for (int i = 0; i < n; i++) {
+        for (int c = 0; c < 26; c++) {
+            if (i > 0 && 'a' + c != s[i])
+                aut[i][c] = aut[pi[i-1]][c];
+            else
+                aut[i][c] = i + ('a' + c == s[i]);
+        }
+    }
 }
 
 
